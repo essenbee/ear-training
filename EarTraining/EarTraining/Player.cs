39b868dd586fcd1df2a4b8be04a7df4f93f50cfa
Stuart Bonham <stuart_bonham@blueyonder.co.uk>
2016-04-27 11:21:12 +0100

    Fix playback issue using soundtouch

--- EarTraining/EarTraining/Player.cs
+++ EarTraining/EarTraining/Player.cs
@@ -121,23 +121,12 @@ namespace EarTraining
 
                     while (waveChannel.Position < waveChannel.Length)
                     {
-                        #region Read samples from file
-
-                        // *** Read one chunk from input file ***
                         bytesRead = waveChannel.Read(convertInputBuffer.Bytes, 0, convertInputBuffer.Bytes.Length);
-                        // **************************************
 
                         floatsRead = bytesRead / ((sizeof(float)) * format.Channels);
 
-                        #endregion
-
                         if (waveChannel.CurrentTime > waveChannel.TotalTime)
-                        {
-                            #region Flush left over samples
-
-                            // ** End marker reached **
-                            // Now the input buffer is processed, 'flush' some last samples that are
-                            // hiding in the SoundTouch's internal processing pipeline.
+                        { 
                             _s.Clear();
                             _s.Flush();
                             waveChannel.Flush();
@@ -153,21 +142,11 @@ namespace EarTraining
                                     inputProvider.AddSamples(convertOutputBuffer.Bytes, 0, (int)samplesProcessed * sizeof(float) * format.Channels, currentBufferTime);
                                 }
                             }
-
-
-                            #endregion
-
                         }
-
-                        #region Put samples in SoundTouch
 
                         // ***                    Put samples in SoundTouch                   ***
                         _s.PutSamples(convertInputBuffer.Floats, (uint)floatsRead);
-                        // **********************************************************************
 
-                        #endregion
-
-                        #region Receive & Play Samples
                         // Receive samples from SoundTouch
                         do
                         {
@@ -179,126 +158,15 @@ namespace EarTraining
                             if (samplesProcessed > 0)
                             {
                                 TimeSpan currentBufferTime = waveChannel.CurrentTime;
-
                                 // ** Play samples that came out of SoundTouch by adding them to AdvancedBufferedWaveProvider - the buffered player 
                                 inputProvider.AddSamples(convertOutputBuffer.Bytes, 0, (int)samplesProcessed * sizeof(float) * format.Channels, currentBufferTime);
-                                // **********************************************************************
-
-                                // Wait for queue to free up - only then add continue reading from the file
-                                // >> Note: when paused, loop runs infinitely
-                                //while (inputProvider.GetQueueCount() > BusyQueuedBuffersThreshold)
-                                //{
-                                //    Thread.Sleep(10);
-                                //}
                                 bufferIndex++;
                             }
                         } while (samplesProcessed != 0);
-                        #endregion
-                    } // while
-
-
-
-                    //// Now the input file is processed, yet 'flush' few last samples that are
-                    //// hiding in the SoundTouch's internal processing pipeline.
-                    //_soundTouch.Flush();
-                    //_s.Flush();
-
-                    //do
-                    //{
-                    //    nSamples = _soundTouch.ReceiveSamples(sampleBuffer, buffSizeSamples);
-                    //    sSamples = _s.ReceiveSamples(sBuffer, sBuffSizeSamples);
-
-                    //    outFile.Write(sBuffer, nSamples * channels);
-                    //    //outFile.Write(sampleBuffer, nSamples * channels);
-                    //    //} while (nSamples != 0);
-                    //} while (sSamples != 0);
-
-
-                    //// =============================================================================================
-
-                    //var inFile = new WavInFile(Resources.ResourceManager.GetStream(chord.GetAudioResourceName(numStrums),
-                    //         CultureInfo.InvariantCulture));
-                    //var bits = inFile.GetNumBits();
-                    //var sampleRate = inFile.GetSampleRate();
-                    //var channels = inFile.GetNumChannels();
-                    //var outFile = new WavOutFile(new MemoryStream(), sampleRate, bits, channels);
-
-                    //_soundTouch.SetSampleRate(sampleRate);
-                    //_soundTouch.SetChannels(channels);
-
-                    //_soundTouch.SetTempoChange(tempoChange);
-                    //_soundTouch.SetPitchSemiTones(0.0f);
-                    //_soundTouch.SetRateChange(0.0f);
-                    //_soundTouch.SetSetting(SettingId.UseQuickseek, 1);
-                    //_soundTouch.SetSetting(SettingId.UseAntiAliasFilter, 0);
-
-                    //_s.SetSampleRate(sampleRate);
-                    //_s.SetChannels(channels);
-
-                    //_s.SetTempoChange(tempoChange);
-                    //_s.SetPitchSemiTones(0.0f);
-                    //_s.SetRateChange(0.0f);
-                    //_s.SetSetting(SoundTouchSharp.SoundTouchSettings.SETTING_USE_QUICKSEEK, 0);
-                    //_s.SetSetting(SoundTouchSharp.SoundTouchSettings.SETTING_USE_AA_FILTER, 0);
-
-                    //int nSamples;
-                    //uint sSamples;
-                    //var sampleBuffer = new TSampleType[BuffSize];
-                    //var sBuffer = new float[BuffSize];
-                    //var buffSizeSamples = BuffSize / channels;
-                    //var sBuffSizeSamples = (uint)(BuffSize / channels);
-
-                    //// Process samples read from the input file
-                    //while (!inFile.Eof())
-                    //{
-                    //    // Read a chunk of samples from the input file
-                    //    //var num = inFile.Read(sampleBuffer, BuffSize);
-
-                    //    var num = inFile.Read(sBuffer, BuffSize);
-
-                    //    nSamples = num / inFile.GetNumChannels();
-                    //    sSamples = (uint)(num / inFile.GetNumChannels());
-
-                    //    // Feed the samples into SoundTouch processor
-                    //    _soundTouch.PutSamples(sampleBuffer, nSamples);
-                    //    _s.PutSamples(sBuffer, sSamples);
-
-                    //    // Read ready samples from SoundTouch processor & write them output file.
-                    //    // NOTES:
-                    //    // - 'receiveSamples' doesn't necessarily return any samples at all
-                    //    //   during some rounds!
-                    //    // - On the other hand, during some round 'receiveSamples' may have more
-                    //    //   ready samples than would fit into 'sampleBuffer', and for this reason 
-                    //    //   the 'receiveSamples' call is iterated for as many times as it
-                    //    //   outputs samples.
-                    //    do
-                    //    {
-                    //        nSamples = _soundTouch.ReceiveSamples(sampleBuffer, buffSizeSamples);
-                    //        sSamples = _s.ReceiveSamples(sBuffer, sBuffSizeSamples);
-
-                    //        outFile.Write(sBuffer, nSamples * channels);
-                    //        //outFile.Write(sampleBuffer, nSamples * channels);
-                    //        //} while (nSamples != 0);
-                    //    } while (sSamples != 0);
-                    //}
-
-                    //// Now the input file is processed, yet 'flush' few last samples that are
-                    //// hiding in the SoundTouch's internal processing pipeline.
-                    //_soundTouch.Flush();
-                    //_s.Flush();
-
-                    //do
-                    //{
-                    //    nSamples = _soundTouch.ReceiveSamples(sampleBuffer, buffSizeSamples);
-                    //    sSamples = _s.ReceiveSamples(sBuffer, sBuffSizeSamples);
-
-                    //    outFile.Write(sBuffer, nSamples * channels);
-                    //    //outFile.Write(sampleBuffer, nSamples * channels);
-                    //    //} while (nSamples != 0);
-                    //} while (sSamples != 0);
+                    }
 
                     inputProviders.Add(inputProvider);
-                    //inFile?.Dispose();
+
                 }
 
                 _s.Dispose();
@@ -320,12 +188,13 @@ namespace EarTraining
                     audioOutput.Init(input);
                     audioOutput.Play();
 
-                    while (audioOutput.PlaybackState != PlaybackState.Stopped)
-                    {
-                        Thread.Sleep(20);
-                    }
+                  //while (audioOutput.PlaybackState != PlaybackState.Stopped)
+                  while(input.GetQueueCount() > 0)
+                  {
+                       Thread.Sleep(20);
+                  }
 
-                    audioOutput.Stop();
+                  audioOutput.Stop();
                 }
             }
             else
@@ -361,38 +230,6 @@ namespace EarTraining
             else
             {
                 logger.Debug("    * soundFile was null - check Resources!");
-            }
-        }
-
-        private void PlayAudioResource(WavOutFile outFile)
-        {
-            if (outFile != null)
-            {
-                using (outFile)
-                {
-                    using (var wfr = new WaveFileReader(outFile.GetStream()))
-                    {
-                        using (WaveChannel32 wc = new WaveChannel32(wfr) { PadWithZeroes = false })
-                        {
-                            using (var audioOutput = new DirectSoundOut())
-                            {
-                                audioOutput.Init(wc);
-                                audioOutput.Play();
-
-                                while (audioOutput.PlaybackState != PlaybackState.Stopped)
-                                {
-                                    Thread.Sleep(20);
-                                }
-
-                                audioOutput.Stop();
-                            }
-                        }
-                    }
-                }
-            }
-            else
-            {
-                logger.Debug("    * outFile was null - check Resources!");
             }
         }
     }
